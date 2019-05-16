b23d42e4c525af73f7d45741eda9c096b44fb618
testsmile <2232824543@qq.com>
2019-05-16 11:49:07 +0800

    留言

---
+++ database/migrations/2019_05_16_034518_create_data_private_letters_content_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataPrivateLettersContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_private_letters_content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_private_letters_content');
+    }
+}
---
+++ database/migrations/2019_05_16_034557_create_data_private_letters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataPrivateLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_private_letters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('sender_guid', 32);
+            $table->char('receiver_guid', 32);
+            $table->integer('content_id');
+            $table->smallInteger('is_read')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_private_letters');
+    }
+}
