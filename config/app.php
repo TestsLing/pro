c72ef4bbe3b9990d2696c9ef68b5321ee1c58b02
testsmile <2232824543@qq.com>
2019-05-17 11:58:46 +0800

    laravel-swoole

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\ModelServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        SwooleTW\Http\LaravelServiceProvider::class,
 
     ],
 
